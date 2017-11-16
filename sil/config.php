2da132a2c8805733b766f715ba7715c0c5dd36a3
andreezw <andreezw1021@gmail.com>
2017-11-15 21:23:36 -0500

    Subo la estructura del proyecto al repositorio de GitHub

---
+++ sil/config.php
@@ -0,0 +1,14 @@
+<?php
+$servidor = '97.74.31.5';
+$bd = 'fenedif2';
+$usuario_mysql = 'fenedif2';
+$clave_mysql = 'kJwyArt#35gSned';
+$administrador = '7777777';
+$nombre_centro = 'Universidad Tecnológica "Indoamérica"';
+$dir_centro = 'Ambato';
+$telefono_centro = '032421452';
+$fax_centro = '032421713';
+$mail_centro = 'info@uti.edu.ec';
+$web_centro = 'http://simons.uti.edu.ec';
+$idioma = 'castellano.php';
+?>
